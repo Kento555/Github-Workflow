1bf20881073c377e1adb508876e906df59efac3e
Kento555 <shen_kento555@hotmail.com>
2025-04-01 19:19:06 +0800

    change s3

--- s3.tf
+++ s3.tf
@@ -1,9 +1,17 @@
 # Create AWS S3
-resource "aws_s3_bucket" "ws_bucket_github" {
-  bucket = "my-tf-test-with-github-action-bucket" 
+# resource "aws_s3_bucket" "ws_bucket_github" {
+#   bucket = "my-tf-test-with-github-action-bucket" 
 
-  tags = {
-    Name        = "WS Bucket"
-    Environment = "Dev"
-  }
+#   tags = {
+#     Name        = "WS Bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+resource "aws_s3_bucket" "ws_bucket_github" {
+  bucket = "my-tf-test-with-github-action-bucket-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
 }
