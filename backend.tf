1cd3994291aac40abce48f8704dc68628ce2fbcb
Kento555 <shen_kento555@hotmail.com>
2025-04-01 19:05:34 +0800

    added s3 terraform code

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# Backend config to store tfstate in an S3 bucket
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce9-tfstate"                    # This is an existing bucket to store terraform tfstate file
+    key    = "ws-cf-s3.tfstate"                    # Replace the value of key to <your suggested name>.tfstate 
+    region = "us-east-1"
+    encrypt = true
+  }
+}
