070c881c645d8ed20945c8586382264855619a92
Kento555 <shen_kento555@hotmail.com>
2025-04-01 21:01:06 +0800

    revise

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create AWS S3
 resource "aws_s3_bucket" "ws_bucket_github" {
-  bucket = "ws-github-action-bucket" 
+  bucket = "ws-github-action-bucket-2" 
 
   tags = {
     Name        = "WS Bucket"
